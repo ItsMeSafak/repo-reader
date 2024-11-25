4478e308267c1549e3af9fa3959aae09d120178b
Safak Inan <safak.inan61@hotmail.com>
2024-11-25 23:53:27 +0100

    Initial commit

---
+++ src/main/java/com/example/ballsupinyoface/BallsupinyofaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ballsupinyoface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallsupinyofaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BallsupinyofaceApplication.class, args);
+    }
+
+}
